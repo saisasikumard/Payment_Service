c91fc869d5a86de9e024636233333edafd012f15
saisasikumard~ <saikumardhavileswarapu@gmail.com>
2024-11-03 01:30:41 +0530

    Enabling Eureka Service

---
+++ src/main/java/com/example/Payment_Service/repository/PaymentRepo.java
@@ -0,0 +1,10 @@
+package com.example.Payment_Service.repository;
+
+import com.example.Payment_Service.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepo extends JpaRepository<Payment,Integer> {
+
+}
---
+++ src/main/java/com/example/Payment_Service/service/PaymentService.java
@@ -0,0 +1,26 @@
+package com.example.Payment_Service.service;
+
+import com.example.Payment_Service.entity.Payment;
+import com.example.Payment_Service.repository.PaymentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.UUID;
+
+@Service
+public class PaymentService {
+    @Autowired
+    PaymentRepo paymentRepo;
+
+    public Payment doPayment(Payment paymentInput){
+        Payment payment=paymentInput;
+        payment.setTranscationId(UUID.randomUUID().toString());
+        payment.setPaymentStatus(thirdPartypaymentApi());
+        return paymentRepo.save(payment);
+    }
+    //3rd party api call
+    public String thirdPartypaymentApi(){
+        return new Random().nextBoolean()?"Success":"Fail";
+    }
+}
